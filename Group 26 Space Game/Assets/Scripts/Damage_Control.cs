e08fd1b559aeb3c0056f642f663bbff870349122
RyanHaze <Rhazewski@gmail.com>
2018-02-08 08:15:58 -0500

    Good starting point
    Added ship/enemies found free to use art plus some unity asset store things

---
+++ Group 26 Space Game/Assets/Scripts/Damage_Control.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage_Control : MonoBehaviour {
+
+   // set Health
+   public int health = 1;
+
+   //invulnerability 
+   float invulnTime = 0;
+   public float invulnPeriod = 0;
+
+    //layer integer
+    int correctLayer;
+    void Start()
+    {
+        correctLayer = gameObject.layer;  
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger");
+
+        //check for invlun
+        //if (invuln <= 0)
+        
+        health--;
+        invulnTime = invulnPeriod;
+
+        //set invulnerability time
+        gameObject.layer = 10;       
+    }
+
+    void Update()
+    {
+        invulnTime -= Time.deltaTime;
+        if (invulnTime <= 0)
+        {
+            gameObject.layer = correctLayer;
+        }
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Group 26 Space Game/Assets/Scripts/Player_Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shooting : MonoBehaviour {
+    public GameObject bulletPrefab;
+    //create a cooldown for shooting
+    float cooldownTimer = 0;
+    public float fireDelay = 5f;
+
+    //make adjustable where bullets come from ship
+    public Vector3 bulletOffset = new Vector3(0, .5f, 0);
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+	    if( Input.GetButtonDown("Fire1") && cooldownTimer <= 0)
+        {
+            //shoot stuff
+            Debug.Log("SHOOTING!");
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset;
+
+            Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+        }
+	}
+}
