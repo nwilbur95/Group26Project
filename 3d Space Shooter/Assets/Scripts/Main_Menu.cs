2458db480a555043c44e0af68b30aa117dda8ad0
nwilbur95 <nickwilbur95@gmail.com>
2018-02-15 14:40:34 -0500

    Added basic menu
    Added Start, Settings, and Quit button. Will add Load and Continue game buttons and menus as those features are implemented.

---
+++ 3d Space Shooter/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+
+	//Given that this we don't have saving implemented yet, this will just load the "Level_1" scene
+	public void StartGame ()
+	{
+		//Load the next scene in the index
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+		Debug.Log ("QUIT!");
+	}
+
+}
---
+++ 3d Space Shooter/Assets/Tutorial Assets/Scipts/destroy_by_boundry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_by_boundry : MonoBehaviour
+{
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
