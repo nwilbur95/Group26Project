f11e3a0a67c028c3c79311eb5814618de885955d
Amil Patel <34750446+amil487@users.noreply.github.com>
2018-03-01 12:04:33 -0500

    My sprint 1 stuff, actually legit (promise) (#10)
    * Sprint 1 final
    
    * Sprint 1 final

--- 3d Space Shooter/Assets/Scripts/Destroy_By_COntact.cs
+++ 3d Space Shooter/Assets/Scripts/Destroy_By_COntact.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Destroy_By_COntact : MonoBehaviour {
@@ -7,6 +8,8 @@ public class Destroy_By_COntact : MonoBehaviour {
 
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int health = 100;
+    public Text playerHealth;
 
     void OnTriggerEnter(Collider other)
     {
@@ -19,13 +22,28 @@ public class Destroy_By_COntact : MonoBehaviour {
         if(explosion != null)
         {
             Instantiate(explosion, transform.position, transform.rotation);
+            health--;
+            if (health == 0)    //object dead
+            {
+                Destroy(gameObject);
+            }
+            playerHealth.text = "Health: " + health.ToString();     //update health UI (make sure UI attached to player ship health variable)
         }
         // If contact was with player, add the unique player explosion effect, destroy object.
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            health--;
+            if (health == 0)    //object dead
+            {
+                Destroy(gameObject);
+            }
         }
-        Destroy(other.gameObject);
-        Destroy(gameObject);
+
+        if(other.tag == "asteroid")     //if contact hit with an asteroid, we want the asteroid to explode
+        {
+            Destroy(other.gameObject);
+        }
+
     }
 }
