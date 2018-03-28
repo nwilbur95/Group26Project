dd0ba6da521d83711804916a6b2582c65d623ba0
grymestone <grymestone@ufl.edu>
2018-03-28 18:52:22 -0400

    test commit

---
+++ 3d Space Shooter/Assets/Scripts/Item_Based_Scripts/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static Inventory instance;
+
+    public int space = 20;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory found");
+            return;
+        }
+
+        instance = this;
+
+
+    }
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (items.Count >= space)
+        {
+            Debug.Log("Not enough room");
+            return false;
+        }
+        items.Add(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+            return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+
+
+}
---
+++ 3d Space Shooter/Assets/Scripts/Item_Based_Scripts/Item_Interaction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Interaction : MonoBehaviour {
+
+    public Item item;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //this is a workaround to prevent ship from losing health on pickup
+            Player_Controller.health = Player_Controller.health + 1;
+            
+            bool wasPickedUp = Inventory.instance.Add(item);
+
+            if (wasPickedUp)
+            {
+                Debug.Log("we Interacted with: " + item.name);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
