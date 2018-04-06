af9bb8e6bf926d5da70a2b74c91dd74df1fc0f16
Amil Patel <34750446+amil487@users.noreply.github.com>
2018-04-06 15:45:37 -0400

    Amil's Sprint 2  (#16)
    * Sprint 1 final
    
    * Scrap asset added
    
    * Sprint 2
    
    * AMIL SPRINT 2: Add trading system
    
    * moved coin prefab
    
    * bork
    
    * Simple folder edits
    
    * Revert "Simple folder edits"
    
    This reverts commit 780f40249797ec3e3db94d72712570c04cdfc67a.
    
    * return of the good stuff
    
    should revert to simple folder edits commit
    
    * Revert "Sprint 2"
    
    This reverts commit 1553975515722c40ace0e2984e30e5ce091460d3.
    
    * Revert "Revert "Sprint 2""
    
    This reverts commit 81ea17ddcd2a18e0f9767c8378a8f06157769965.
    
    * Revert "return of the good stuff"
    
    This reverts commit bb77474f55244b126e35ec7b48f8de2703f0fa90.
    
    * Revert "Revert "Simple folder edits""
    
    This reverts commit 26bc8dcebb6d6bb7688801cf899e40d293ea3b21.
    
    * Revert "Merge branch 'master' into master"
    
    This reverts commit f614309a073e8a00a93f3ffdb11066f236b94cc2, reversing
    changes made to 0d2cdb6e3be2a1a1e1f66ff177b32dc13f0238a8.
    
    * Revert "Merge branch 'master' of https://github.com/amil487/Group26Project"
    
    This reverts commit 0d2cdb6e3be2a1a1e1f66ff177b32dc13f0238a8, reversing
    changes made to e20ca76434fe345a1d74421ea3c2d8685c567ecd.
    
    * Sprint 2 (actually good)
    
    After all the reverts, this is all good.
    
    * Scene change
    
    * Download original scene files

--- 3d Space Shooter/Assets/Scripts/EnemyController.cs
+++ 3d Space Shooter/Assets/Scripts/EnemyController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
  using UnityEngine.UI;
 
-[System.Serializable]
+
 public class EnemyController : MonoBehaviour
 {
 	// public GameObject enemy;
@@ -10,50 +10,29 @@ public class EnemyController : MonoBehaviour
 	public float Min;
 	public float MoveSpeed;
 
-	//Shooting variables
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-
-	private AudioSource audioSource;
-	private float dist;
-	private float nextFire;
 
 	void Start()
 	{
-		audioSource = GetComponent<AudioSource> ();
+		InvokeRepeating("Update", 1, 1);
 	}
 
 	void Update()
 	{
-		//Distance from player
-		dist = Vector3.Distance(Player.position, transform.position);
-		Debug.Log (dist);
-
-		if(dist <= Max)
-		{
-			transform.LookAt(Player);
+		transform.LookAt(Player);
+			if(Vector3.Distance(transform.position, Player.position) >= Min)
+			{
+				// Debug.Log(transform.forward);
+				if (transform.forward == new Vector3(0,0,0))
+				{
+					transform.forward = new Vector3(0, 0, 1);
+				}
+				transform.position += transform.forward*MoveSpeed*Time.deltaTime;
 			
-			// Debug.Log(transform.forward);
-			if (transform.forward == new Vector3(0,0,0))
-			{
-				transform.forward = new Vector3(0, 0, 1);
-
+				if(Vector3.Distance(transform.position, Player.position) <= Max)
+				{
+					return;
+				}
 			}
 
-			transform.position += transform.forward*MoveSpeed*Time.deltaTime;
-
-			if (Time.time > nextFire) 
-			{
-				nextFire = Time.time + fireRate;
-				Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-				audioSource.Play();
-			}
-
-			if(dist <= Min)
-			{
-				return;
-			}							
-		}			
 	}
 }
