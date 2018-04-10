0f6147b999ddfef54744df207f6af166b270a4fe
nwilbur95 <nickwilbur95@gmail.com>
2018-04-10 12:00:34 -0400

    Added spawn points prefab and spawner script

---
+++ 3d Space Shooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public static int activeEnemies;
+	public GameObject enemyShip;
+
+	private Transform[] spawnPoints;
+	private int randomSpawn;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawnPoints = GetComponentsInChildren<Transform> ();
+		randomSpawn = Random.Range (0, spawnPoints.Length);
+		Transform spawnPoint = spawnPoints [randomSpawn];
+		Instantiate (enemyShip, spawnPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
